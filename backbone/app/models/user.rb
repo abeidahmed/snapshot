401be3c24ccc9c373169c167b4913dccf6efa2d4
abeidahmed <abeidahmed92@gmail.com>
2021-04-06 00:29:16 +0530

    Add user model validations

--- backbone/app/models/user.rb
+++ backbone/app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :full_name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } # rubocop:disable Layout/LineLength
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_validation :strip_email
+  before_save :lowercase_email
+
+  private
+
+  def strip_email
+    self.email = email.to_s.strip
+  end
+
+  def lowercase_email
+    self.email = email.to_s.downcase
+  end
 end
--- backbone/spec/models/user_spec.rb
+++ backbone/spec/models/user_spec.rb
@@ -1,5 +1,43 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe "validations" do
+    it { is_expected.to have_secure_password }
+
+    it { is_expected.to validate_presence_of(:email) }
+
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
+    it { is_expected.to validate_length_of(:email).is_at_most(255) }
+
+    it { is_expected.to allow_value("johndoe@example.com", "johjn@exa.co.in").for(:email) }
+
+    it { is_expected.not_to allow_value("johndoeexample.com", "johjn@exa").for(:email) }
+
+    it { is_expected.to validate_presence_of(:full_name) }
+
+    it { is_expected.to validate_length_of(:full_name).is_at_most(255) }
+
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe "callbacks" do
+    it "lowercases email before saving" do
+      email = "hello@example.com"
+      user.email = email.upcase
+      user.save!
+
+      expect(user.email).to eq(email)
+    end
+
+    it "strips the email before validation" do
+      email = "   hello@example.com "
+      user.email = email
+      user.save!
+
+      expect(user.email).to eq("hello@example.com")
+    end
+  end
 end
