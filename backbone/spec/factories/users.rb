0dd24bc942cf4768795b967af39010101ba3cdd4
abeidahmed <abeidahmed92@gmail.com>
2021-04-06 00:22:51 +0530

    Configure user factory

--- backbone/spec/factories/users.rb
+++ backbone/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    full_name { "MyString" }
-    email { "MyString" }
-    password_digest { "MyString" }
+    full_name { "John Doe" }
+    sequence(:email) { |n| "johndoe#{n}@example.com" }
+    password { "secretpassword" }
   end
 end
