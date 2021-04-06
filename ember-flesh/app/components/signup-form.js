ab7f98550a29a01b9dafc1d96a6b7ae7224bef34
abeidahmed <abeidahmed92@gmail.com>
2021-04-06 12:04:03 +0530

    Successfully post user data from server

---
+++ ember-flesh/app/components/signup-form.js
@@ -0,0 +1,43 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class SignupFormComponent extends Component {
+  @service store;
+
+  @tracked email = '';
+  @tracked password = '';
+  @tracked fullName = '';
+
+  @action
+  updateEmailField(event) {
+    this.email = event.target.value;
+  }
+
+  @action
+  updatePasswordField(event) {
+    this.password = event.target.value;
+  }
+
+  @action
+  updateNameField(event) {
+    this.fullName = event.target.value;
+  }
+
+  @action
+  async createUserRecord(event) {
+    event.preventDefault();
+    const user = this.store.createRecord('user', {
+      full_name: this.fullName,
+      email: this.email,
+      password: this.password,
+    });
+
+    try {
+      await user.save();
+    } catch (_error) {
+      console.log(user.errors.get('email')[0].message);
+    }
+  }
+}
---
+++ ember-flesh/app/models/user.js
@@ -0,0 +1,7 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class UserModel extends Model {
+  @attr('string') email;
+  @attr('string') full_name;
+  @attr('string') password;
+}
