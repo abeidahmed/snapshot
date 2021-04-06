1525145255479efb840cdd7ef20c7960042c42c0
abeidahmed <abeidahmed92@gmail.com>
2021-04-06 23:59:41 +0530

    Create snapshot card

--- ember-flesh/tailwind.config.js
+++ ember-flesh/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: [],
@@ -8,10 +9,19 @@ module.exports = {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        rose: colors.rose,
+      },
+      spacing: {
+        image: '42%',
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['group-hover'],
+      borderWidth: ['first'],
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
