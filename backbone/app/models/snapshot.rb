1f045aee367ce84b7edff0fa42332c49212d9f4a
abeidahmed <abeidahmed92@gmail.com>
2021-04-08 19:14:18 +0530

    Add snapshot model validations

--- backbone/app/models/snapshot.rb
+++ backbone/app/models/snapshot.rb
@@ -1,3 +1,11 @@
 class Snapshot < ApplicationRecord
   belongs_to :user
+
+  VALID_URL_REGEX = /\A(http|https):\/\/[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$\z/ix
+
+  validates :url, presence: true, format: { with: VALID_URL_REGEX }
+  validates :image_url, format: { with: VALID_URL_REGEX }, allow_blank: true
+  validates :title, presence: true
+
+  enum visibility: { universal: "universal", listed: "listed" }
 end
--- backbone/spec/models/snapshot_spec.rb
+++ backbone/spec/models/snapshot_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe Snapshot, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:url) }
+
+    it do
+      expect(snapshot).to allow_value(
+        "https://goog.com", "https://www.hello.com", "http://gool.com", "http://www.googl.co.in"
+      ).for(:url)
+    end
+
+    it { is_expected.not_to allow_value("https://goog", "www.hello.com").for(:url) }
+
+    it do
+      expect(snapshot).to allow_value(
+        "https://goog.com", "https://www.hello.com", "http://gool.com", "http://www.googl.co.in", ""
+      ).for(:image_url)
+    end
+
+    it { is_expected.not_to allow_value("https://goog", "www.hello.com").for(:image_url) }
+
+    it { is_expected.to validate_presence_of(:title) }
+
+    it { is_expected.to define_enum_for(:visibility).with_values(universal: "universal", listed: "listed").backed_by_column_of_type(:string) } # rubocop:disable Layout/LineLength
+  end
 end
