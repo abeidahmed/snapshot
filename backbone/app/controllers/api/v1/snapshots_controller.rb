183f5edc8c417e58046985f758981d085e6a8169
abeidahmed <abeidahmed92@gmail.com>
2021-04-08 19:57:54 +0530

    Create snapshots_controller create action

--- backbone/app/controllers/api/v1/snapshots_controller.rb
+++ backbone/app/controllers/api/v1/snapshots_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::SnapshotsController < ApplicationController
+  def create
+    authorize Snapshot.new
+
+    snapshot = current_user.snapshots.build(snapshot_new_params)
+    if snapshot.save
+      # do
+    else
+      render json: { errors: snapshot.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def snapshot_new_params
+    params.require(:snapshot).permit(:url, :visibility)
+  end
 end
--- backbone/config/routes.rb
+++ backbone/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[create]
       resources :sessions, only: %i[create]
+      resources :snapshots, only: %i[create]
     end
   end
 end
