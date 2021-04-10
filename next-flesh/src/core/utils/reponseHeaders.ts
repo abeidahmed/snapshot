4237a6f2e344468af61db64a07ea99f4868ac0cb
abeidahmed <abeidahmed92@gmail.com>
2021-04-10 15:26:21 +0530

    Setup axios headers

---
+++ next-flesh/src/core/utils/reponseHeaders.ts
@@ -0,0 +1,10 @@
+export function responseHeaders() {
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+  };
+
+  return config;
+}
