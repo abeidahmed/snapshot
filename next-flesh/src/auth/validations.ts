32dc76b68676b76c3ce1bd7f6ce1207f47f6a868
abeidahmed <abeidahmed92@gmail.com>
2021-04-10 19:18:37 +0530

    Change validation texts

--- next-flesh/src/auth/validations.ts
+++ next-flesh/src/auth/validations.ts
@@ -1,14 +1,24 @@
 import * as yup from "yup";
 
-const email = yup.string().email().required();
+const email = yup
+  .string()
+  .email("Email must be valid")
+  .required("Email is required");
 
 export const signinValidations = yup.object().shape({
   email,
-  password: yup.string().required(),
+  password: yup.string().required("Password is required"),
 });
 
 export const signupValidations = yup.object().shape({
   email,
-  password: yup.string().max(72).required(),
-  full_name: yup.string().max(255).required(),
+  password: yup
+    .string()
+    .min(6, "Password is too short (min 6 characters)")
+    .max(72, "Password is too long (max 72 characters)")
+    .required("Password is required"),
+  full_name: yup
+    .string()
+    .max(255, "Full name is too long (max 255 characters)")
+    .required("Full name is required"),
 });
