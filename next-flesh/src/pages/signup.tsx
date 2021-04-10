bc4874a0455dd2756b144a8038807d35daeedf19
abeidahmed <abeidahmed92@gmail.com>
2021-04-10 12:46:15 +0530

    Add icon

--- next-flesh/src/pages/signup.tsx
+++ next-flesh/src/pages/signup.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import SignupForm from "auth/components/SignupForm";
+import Icon from "core/components/Icons";
 
 const Signup: React.FC = () => {
   return (
     <main className="flex items-center justify-center min-h-screen py-12 bg-gray-100 sm:px-6 lg:px-8">
       <div className="w-full">
         <div className="px-4">
+          <Icon
+            icon="logo"
+            viewBox="0 0 76 19"
+            className="h-6 w-auto mx-auto text-gray-700"
+          />
           <h1 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
             Create an account
           </h1>
