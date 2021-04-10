b23e36300b565cd5c2b572e0035eb2cc16c7a91a
abeidahmed <abeidahmed92@gmail.com>
2021-04-10 12:24:03 +0530

    Switch tags

--- next-flesh/src/pages/signup.tsx
+++ next-flesh/src/pages/signup.tsx
@@ -7,9 +7,9 @@ const Signup: React.FC = () => {
     <main className="flex items-center justify-center min-h-screen py-12 bg-gray-100 sm:px-6 lg:px-8">
       <div className="w-full">
         <div className="px-4">
-          <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
+          <h1 className="mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
             Create an account
-          </h2>
+          </h1>
         </div>
         <div className="w-full sm:max-w-md mx-auto">
           <div className="px-10 py-8 mt-8 bg-white shadow sm:rounded-md">
