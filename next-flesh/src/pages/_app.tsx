0c949e0419b974810832e3551fa03e63eefafdfa
abeidahmed <abeidahmed92@gmail.com>
2021-04-10 14:15:17 +0530

    Setup QueryClientProvider

--- next-flesh/src/pages/_app.tsx
+++ next-flesh/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "styles/fonts.scss";
 import "styles/index.scss";
 
+const queryClient = new QueryClient();
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
