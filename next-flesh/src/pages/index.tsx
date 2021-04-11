f2b1a3a660c4ff1e8cce857163029fc12701f30a
abeidahmed <abeidahmed92@gmail.com>
2021-04-11 12:12:23 +0530

    Increase col width on md screen

--- next-flesh/src/pages/index.tsx
+++ next-flesh/src/pages/index.tsx
@@ -10,14 +10,14 @@ const Home: React.FC = () => {
     <div className="max-w-7xl mx-auto flex">
       <Sidebar />
       <main className="md:grid grid-cols-12 flex-1 min-h-screen">
-        <div className="col-span-7 border-r">
+        <div className="md:col-span-8 lg:col-span-7 border-r">
           <Header />
           <section className="space-y-2 bg-gray-100">
             <Snapshot />
             <Snapshot />
           </section>
         </div>
-        <div className="col-span-5 pl-6 pr-4 hidden lg:block">
+        <div className="md:col-span-4 lg:col-span-5 pl-6 pr-4 hidden lg:block">
           <div className="py-4 flex items-center justify-end sticky top-0 space-x-5">
             <Modal
               title="Share your snapshot"
