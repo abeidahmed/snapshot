1c852d2feecdc7e0bdf4b1f2ffa1657a82b78dec
abeidahmed <abeidahmed92@gmail.com>
2021-04-11 11:39:13 +0530

    Change default response text

--- next-flesh/src/snapshot/validations.ts
+++ next-flesh/src/snapshot/validations.ts
@@ -1,6 +1,9 @@
 import * as yup from "yup";
 
 export const snapshotNewValidations = yup.object().shape({
-  website_url: yup.string().url().required(),
+  website_url: yup
+    .string()
+    .url("Website URL must be a valid URL")
+    .required("Website URL is required"),
   visibility: yup.string().oneOf(["universal", "listed"]).required(),
 });
